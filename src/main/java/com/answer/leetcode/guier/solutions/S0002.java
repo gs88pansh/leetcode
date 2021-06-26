9ada39db5119c4ddfbb90f4081c20871c2ba75cb
gs88pansh <gy.guiyue@qq.com>
2021-06-26 21:40:01 +0800

    feat: 最小无重复子串

---
+++ src/main/java/com/answer/leetcode/guier/solutions/S0002.java
@@ -0,0 +1,33 @@
+package com.answer.leetcode.guier.solutions;
+
+/**
+ * @see <a href="https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/submissions/">无重复的最长子串</>
+ *
+ * 类型：解题思路滑动窗口
+ */
+public class S0002 {
+    public int lengthOfLongestSubstring(String s) {
+        int[] numsOfChars = new int[1028];
+        int leftIndex = 0;
+        int maxNum = 0;
+        int currentNum = 0;
+        for (int i = 0; i < s.length(); i ++) {
+            char charNum = s.charAt(i);
+            numsOfChars[charNum] ++;
+            currentNum ++;
+            if (numsOfChars[charNum] > 1) {
+                for (; leftIndex < i && numsOfChars[charNum] > 1; leftIndex ++) {
+                    numsOfChars[s.charAt(leftIndex)] --;
+                    currentNum --;
+                }
+            }
+            maxNum = Math.max(currentNum, maxNum);
+        }
+        return maxNum;
+    }
+
+    public static void main(String[] args) {
+        S0002 s0002 = new S0002();
+        System.out.println(s0002.lengthOfLongestSubstring("pwwkew"));
+    }
+}
