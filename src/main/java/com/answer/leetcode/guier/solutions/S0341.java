fb2d1f43216788708775ba08e66545814694d04a
guiyue <guiyue@bytedance.com>
2021-07-10 20:10:26 +0800

    feat: 341
    Change-Id: I47a1f1c6c4dec9aabdc89bc8fd59fee05030bb5a

--- src/main/java/com/answer/leetcode/guier/solutions/S0341.java
+++ src/main/java/com/answer/leetcode/guier/solutions/S0341.java
@@ -2,18 +2,15 @@ package com.answer.leetcode.guier.solutions;
 
 
 import com.answer.leetcode.guier.models.NestedInteger;
-import com.answer.leetcode.guier.models.NestedIntegeror;
 import com.answer.leetcode.guier.models.NestedIterator;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.answer.leetcode.guier.utils.NestedIntegerBuilder;
 
 public class S0341 {
 
     public static void main(String[] args) {
-        List<NestedInteger> nestedList = new ArrayList<>();
-        nestedList.add(new NestedIntegeror(null, new ArrayList<>()));
-        NestedIterator nestedIterator = new NestedIterator(nestedList);
+        NestedInteger nestedInteger = NestedIntegerBuilder.buildNestedInteger("[1,2,2,[1,2,3,[1,2,3],3],3]");
+        System.out.println(nestedInteger);
+        NestedIterator nestedIterator = new NestedIterator(nestedInteger.getList());
         while (nestedIterator.hasNext()) {
             System.out.println(nestedIterator.next());
         }
